2a21eebfec06fc58b16023802007ca9eab7853f3
heejung <mishel7@naver.com>
2025-09-29 15:11:58 +0900

    [Revert] : 서버 프록시 설정 해제

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        // '/api'로 시작하는 요청을 프록시
-        target: "http://13.209.168.11",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
